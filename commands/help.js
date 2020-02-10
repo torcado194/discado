038581869196c09e3b5b9da48151bf408c6c96f0
torcado <torcado@yahoo.com>
2020-02-09 21:15:12 -0700

    help command

---
+++ commands/help.js
@@ -0,0 +1,92 @@
+module.exports = (bot) => {
+    return {
+        aliases: ['help', 'h'],
+        help: {
+            text: 'displays documentation for all commands or a specific command',
+            examples: [
+                bot.prefix + 'help',
+                bot.prefix + 'help {command}'
+            ]
+        },
+        call,
+        run
+    };
+    
+    function call(message, params){
+        bot.respond(
+            run(message, params[1]),
+            message
+        );
+    }
+    
+    function run(message, command){
+        let sections = [
+            {
+                name: 'description',
+                value: 'inageBot is a meedia bot for making funy inages and other shit.'
+                + '\n use `' + bot.prefix + 'command` to run a command'
+            }
+        ]
+        
+        return new Promise((resolve, reject) => {
+            if(command){
+                let name = command,
+                    m = Object.keys(bot.commands);
+                for(let i = 0; i < m.length; i++){
+                    let command = bot.commands[m[i]];
+                    let hasRights = !command.rights || bot.rights.check(message.member, command.rights);
+                    if(hasRights && command.aliases.includes(name)){
+                        
+                        message.channel.send({embed:{
+                            title: '‣ ' + m[i],
+                            description: [
+                                '```yaml',
+                                command.help.examples.join('\n'),
+                                '```',
+                                command.help.text,
+                                '⸺*aliases*⸺',
+                                command.aliases.map(e=>'`'+e+'`').join(', '),
+                            ].join('\n'),
+                            color: bot.color
+                        }});
+                        
+                        break;
+                    }
+                }
+            } else {
+                let commands = Object.keys(bot.commands).filter(n => {
+                    let c = bot.commands[n];
+                    if(c.hidden){
+                        return false;
+                    }
+                    if(c.rights && bot.rights.check(message.member, c.rights)){
+                        return false;
+                    }
+                    return true;
+                });
+                let segments = bot.longMessage({embed:{
+                    author: {
+                        name: `inageBot version ${bot.VERSION}`,
+                        iconURL: bot.client.user.avatarURL || undefined
+                    },
+                    description: sections.find(s => s.name === 'description').value,
+                    fields: [
+                        {
+                            name: 'commands',
+                            value: commands.map(name => [
+                                '\r**‣ ' + name + '**',
+                                '```YAML',
+                                bot.commands[name].help.examples.join('\n'),
+                                '```' + bot.commands[name].help.text,
+                                '⸺*aliases*⸺',
+                                bot.commands[name].aliases.map(e=>'`'+e+'`').join(', '),
+                            ].join('\n')).join('\n')
+                        }
+                    ],
+                    color: bot.color
+                }}, '\r');
+                segments.forEach(s => message.channel.send(s));
+            }
+        });
+    }
+};
